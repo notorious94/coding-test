15dcb974d92706b184ce76ccb2f1576c63225c72
notorious94 <akib94dhk@gmail.com>
2022-06-12 21:29:22 +0600

    authentication bug fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, unless: :json_request?
   protect_from_forgery with: :null_session, if: :json_request?
   before_action :authenticate_user!, unless: :json_request?
+  before_action :authorize_api_token
 
   def json_request?
     request.format.json?
   end
+
+  def authorize_api_token
+    return false unless json_request?
+
+    has_valid_access_token = AuthenticateAccessToken.new(request.headers).call
+    return true if has_valid_access_token
+
+    e = APIError::ApiAccessTokenError.new
+    render json: { error: e.message, error_code: e.error_code }, status: 401
+  end
+
 end
